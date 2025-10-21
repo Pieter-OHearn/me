073871b7c862357dc1292a738eee7ee056b286d9
Pieter OHearn <pieter.ohearn@service.nsw.gov.au>
2025-10-21 22:40:54 +0200

    chore: replace sitemap plugin and manually create files

--- vite.config.ts
+++ vite.config.ts
@@ -2,28 +2,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
   base: '/',
   plugins: [
     react(),
-    sitemap({
-      hostname: 'https://www.pieterohearn.com',
-      dynamicRoutes: ['/', '/Pieter-OHearn-CV.pdf'],
-      extensions: ['html', 'pdf'],
-      priority: {
-        '*': 1.0,
-        '/Pieter-OHearn-CV.pdf': 0.6,
-      },
-      changefreq: {
-        '*': 'monthly',
-      },
-      readable: true,
-      generateRobotsTxt: true,
-      robots: [{ userAgent: '*', allow: '/' }],
-      outDir: 'build',
-    }),
   ],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
